d3c0c4bc32a714dabb0d711c1278070ec7f628a5
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-17 12:00:34 -0400

    new commit

--- app/Livewire/Adminpage.php
+++ app/Livewire/Adminpage.php
@@ -2,10 +2,37 @@
 
 namespace App\Livewire;
 
+use App\Models\Blogger;
 use Livewire\Component;
+use Livewire\Features\SupportFileUploads\WithFileUploads;
 
 class Adminpage extends Component
 {
+    use WithFileUploads;
+    public $storyline, $title, $description, $image, $imageName;
+    
+    public function saveImage()
+    {
+        $this->validate([
+            'title' => 'required|string',
+            'storyline' => 'required|string',
+            'description' => 'required|string',
+             'image' => 'image|max:1024' // Adjust validation rules as needed
+        ]);
+
+        // $this->imageName = $this->image->store('images', 'public');
+
+        // Blogger::create([
+        //     'title' => $this->title,
+        //     'storyline' => $this->storyline,
+        //     'description' => $this->description,
+        //     'image' => $this->imageName,
+        // ]);
+
+        // $this->reset('image');
+        // session()->flash('sucess', 'uploaded successfully');
+    }
+
     public function render()
     {
         return view('livewire.adminpage');
--- resources/views/livewire/adminpage.blade.php
+++ resources/views/livewire/adminpage.blade.php
@@ -1,27 +1,49 @@
-<div class="max-w-md mx-auto bg-white p-6 rounded shadow-md">
+<div class="container mx-auto mt-8">
     <div>
-        <form action="" method="POST" enctype="multipart/form-data">
+        <form wire:submit.prevent="upload"  enctype="multipart/form-data">
             @csrf
-    
+            @if (session()->has('success'))
+            <h1>{{session()->get('success')}}</h1>
+        @endif
             <div class="mb-4">
-                <label for="title" class="block text-gray-700 text-sm font-bold mb-2">Title</label>
-                <input type="text" name="title" id="title" class="w-full py-2 px-3 border rounded" placeholder="Enter a title" required>
+                <label for="title" class="block text-gray-700 text-sm font-bold mb-2 w-full">Title</label>
+                <input wire:model="title" type="text" name="title" id="title" class="w-full py-2 px-3 border rounded" placeholder="Enter a title">
+                @error('title')
+                    <small>{{$message}}</small>
+                @enderror
             </div>
     
             <div class="mb-4">
-                <label for="storyline" class="block text-gray-700 text-sm font-bold mb-2">Storyline</label>
-                <input type="text" name="storyline" id="storyline" class="w-full py-2 px-3 border rounded" placeholder="Enter a storyline" required>
+                <label for="storyline" class="block text-gray-700 text-sm font-bold mb-2 w-full">Storyline</label>
+                <input wire:model='storyline' type="text" name="storyline" id="storyline" class="w-full py-2 px-3 border rounded" placeholder="Enter a storyline">
+                @error('storyline')
+                <small>{{$message}}</small>
+            @enderror
             </div>
+           
     
             <div class="mb-4">
-                <label for="description" class="block text-gray-700 text-sm font-bold mb-2">Description</label>
-                <textarea name="description" id="description" class="w-full py-2 px-3 border rounded" rows="4" placeholder="Enter a description" required></textarea>
+                <label for="description" class="block text-gray-700 text-sm font-bold mb-2 w-full">Description</label>
+                <textarea wire:model="description" name="description" id="description" class="w-full py-2 px-3 border rounded" rows="4" placeholder="Enter a description"></textarea>
+                @error('description')
+                <small>{{$message}}</small> 
+             @enderror
             </div>
     
+    
             <div class="mb-4">
-                <label for="image" class="block text-gray-700 text-sm font-bold mb-2">Upload Image</label>
-                <input type="file" name="image" id="image" class="w-full py-2 px-3 border rounded" accept="image/*" required>
-            </div>           
+                <label for="image" class="block text-gray-700 text-sm font-bold mb-2 w-full">Upload Image</label>
+                <input wire:model="image" type="file" name="image" id="image" class="w-full py-2 px-3 border rounded" accept="image/*">
+            @error('image')
+               <small class="text-red-600">{{$message}}</small> 
+            @enderror
+            </div>  
+            
+            <div class="mb-4">
+                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">Submit</button>
+            </div>
         </form>
+        
     </div>
+    
 </div>
