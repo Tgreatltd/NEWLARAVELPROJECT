988283a95c33519fcb353dec3b447b37cf18ed8d
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-17 15:31:37 -0400

    new commit

--- resources/views/livewire/contentpage.blade.php
+++ resources/views/livewire/contentpage.blade.php
@@ -5,8 +5,8 @@
     
             <div class="w-full sm:w-1/3 md:w-1/4 lg:w-1/4 h-[46vh] bg-blue-500 mb-4 p-4">
                 <div><img   src="{{ asset('storage/'. $user->image) }}" alt=""></div>
-               <h1>{{$user->title}}</h1>
-                <h1>{{$user->description}}</h1>
+               <h1 class="text-lg">{{$user->title}}</h1>
+                <h1 style="color: rgb(0, 253, 63)" class="text-lg">{{$user->description}}</h1>
             </div>
 
             @endforeach
