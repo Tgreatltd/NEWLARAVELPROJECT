ad9c2e525f843a7c39f4bb24c13afbe402724988
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-17 10:23:08 -0400

    new commit

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -1,4 +1,4 @@
-{{-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -12,11 +12,4 @@
       @livewire('navbar')
 </div>
 </body>
-</html> --}}
-<x-app-layout>
-
-    <div class="container mx-auto p-4">
-        
-    </div>
-    
-    </x-app-layout>
+</html>
