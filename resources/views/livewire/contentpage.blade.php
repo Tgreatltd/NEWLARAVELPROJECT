44bc7d8e7438ea0ce52929ae2c87dcd075a6957a
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-17 15:11:14 -0400

    new commit

--- resources/views/livewire/contentpage.blade.php
+++ resources/views/livewire/contentpage.blade.php
@@ -3,11 +3,10 @@
         <div class="flex flex-wrap space-x-4">
             @foreach ($users as $user)
     
-            <div class="w-full sm:w-1/4 md:w-1/6 lg:w-1/4 h-40 bg-blue-500 mb-4 p-4">
+            <div class="w-full sm:w-1/3 md:w-1/4 lg:w-1/4 h-40 bg-blue-500 mb-4 p-4">
                 <div><img style="height: 100px; width:100px" src="{{ asset('storage/'. $user->image) }}" alt=""></div>
-                <div>{{$user->title}}</div>
-                <div>{{$user->storyline}}</div>
-                <div>{{$user->description}}</div>
+               <h1>{{$user->title}}</h1>
+                <h1>{{$user->description}}</h1>
             </div>
 
             @endforeach
