331d3568fa7225b39b18ee7b6fe5b275b81896ab
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-17 07:55:37 -0400

    new commit

--- database/migrations/2023_10_17_114802_create_blogadmins.php
+++ database/migrations/2023_10_17_114802_create_blogadmins.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('storyline');
             $table->string('description');
             $table->string('image');
-            $table->string('comment');
             $table->timestamps();
         });
     }
