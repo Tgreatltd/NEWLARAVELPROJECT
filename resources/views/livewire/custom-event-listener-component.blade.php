1069c3d479c3cacf76389b52a9881e3f4c52ec8a
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-19 13:35:37 -0400

    new commit

--- resources/views/livewire/custom-event-listener-component.blade.php
+++ resources/views/livewire/custom-event-listener-component.blade.php
@@ -2,5 +2,5 @@
     {{-- If your happiness depends on money, you will never be happy with yourself. --}}
     <div>
         <p>Received Message: {{ $receivedMessage }}</p>
-    </div> 
+    </div>
 </div>
