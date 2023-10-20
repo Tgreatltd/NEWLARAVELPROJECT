cfe0ca3b8668959be10e208d89cca1f2eabc0053
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-20 14:02:51 -0400

    new commit

--- resources/views/livewire/storyline.blade.php
+++ resources/views/livewire/storyline.blade.php
@@ -1,8 +1,12 @@
 <div>
     {{-- <div>{{$storyId}}</div> --}}
     @if ($storyId)
+    <div><img id="image" class="h-[20vh]" src="{{ asset('storage/'. $storyId->image) }}" alt=""></div>
     <h2>{{ $storyId->title }}</h2>
-    <p>{{ $storyId->storyline }}</p>
+    {{-- <p>{{ $storyId->storyline }}</p> --}}
+    <div class="container mx-auto p-5 bg-green-100">
+        {{$storyId->storyline}}
+    </div>
 @else
     <p>Story not found.</p>
 @endif
