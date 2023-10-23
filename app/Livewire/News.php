1322e3af6d8be8daed05203ee77427dd4822d03a
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-23 19:03:03 -0400

    new commit

--- app/Livewire/News.php
+++ app/Livewire/News.php
@@ -7,12 +7,7 @@ use Livewire\Component;
 
 class News extends Component
 {
-    public $storyId, $id;
-    public function mount($id)
-    {
-        $this->id = $id;
-        $this->storyId = ModelsNews::find($id);
-    }
+    
     public function render()
     {
         return view('livewire.news');
--- app/Livewire/Sports.php
+++ app/Livewire/Sports.php
@@ -7,13 +7,9 @@ use Livewire\Component;
 
 class Sports extends Component
 {
-    public $storyId, $id;
-    public function mount($id){
-        $this->id = $id;
-     $this->storyId=ModelsSports::find($id);
-    }
+
     public function render()
     {
-        return view('livewire.sports');
+        return view('livewire.sports',['users'=>ModelsSports::all()]);
     }
 }
