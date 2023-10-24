36d0b13f10bb11b5118118d0bf47bcfd527a083b
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-24 06:58:34 -0400

    new commit

--- resources/views/livewire/storyline.blade.php
+++ resources/views/livewire/storyline.blade.php
@@ -11,3 +11,19 @@
     <p>Story not found.</p>
 @endif
 </div>
+
+
+<div class="container max-w-screen-lg mx-auto mt-8 ">
+        <div class="flex flex-wrap space-x-4">
+            @foreach ($users as $user)
+    
+            <div class="w-full sm:w-1/3 md:w-1/4 lg:w-1/4 h-[55vh] bg-green-100 mb-4 p-4">
+                <a href="{{url('storyline/'.$user->id)}}" wire:navigate>
+                    <div style=" margin-bottom:60px; height:200px; width:100%"><img style="height:200px; width:100%"   src="{{ asset('storage/'. $user->image) }}" alt=""></div>
+                    <div style="margin-bottom:10px;height:50px;width: 100%;"><h1 class="text-lg">{{$user->title}}</h1></div>
+                  <div style="width: 100%; height:130px;"> <h1 class="text-lg">{{$user->description}}</h1></div>
+                </a>
+            </div>
+
+            @endforeach
+        </div> 
