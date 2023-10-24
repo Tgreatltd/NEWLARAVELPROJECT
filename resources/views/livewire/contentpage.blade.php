90cb03f08eb3b6a617b30e31175b63d5f1d00676
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-24 06:14:05 -0400

    new commit

--- resources/views/livewire/contentpage.blade.php
+++ resources/views/livewire/contentpage.blade.php
@@ -1,9 +1,10 @@
 <div>
-    <div class="container mx-auto  mt-8">
+    {{-- <div class="container mx-auto max-w-screen-lg mt-8"></div> --}}
+    <div class="container mx-auto mt-8 p-2">
         <div class="flex flex-wrap space-x-4">
             @foreach ($users as $user)
     
-            <div class="w-full sm:w-1/3 md:w-1/4 lg:w-1/3 h-[56vh] bg-green-500 mb-4 p-4">
+            <div class="w-full sm:w-1/3 md:w-1/4 lg:w-1/3 h-[55vh] bg-green-500 mb-4 p-4">
                 <a href="{{url('storyline/'.$user->id)}}" wire:navigate>
                     <div style=" margin-bottom:60px; height:200px; width:100%"><img style="height:200px; width:100%"   src="{{ asset('storage/'. $user->image) }}" alt=""></div>
                     <div style="margin-bottom:10px;height:50px;width: 100%;"><h1 class="text-lg">{{$user->title}}</h1></div>
