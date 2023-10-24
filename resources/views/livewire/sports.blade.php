b124a62674fab6705ec6cea1e2e99451c612a514
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-23 23:07:09 -0400

    new commit

--- resources/views/livewire/sports.blade.php
+++ resources/views/livewire/sports.blade.php
@@ -1,15 +1,16 @@
-<div>
-    <div class="container mx-auto p-5">
-        {{-- <div>{{$storyId}}</div> --}}
-        @if ($storyId)
-        <div><img id="image" class="h-[20vh]" src="{{ asset('storage/'. $storyId->image) }}" alt=""></div>
-        <h2>{{ $storyId->title }}</h2>
-        {{-- <p>{{ $storyId->storyline }}</p> --}}
-        <div class="container mx-auto p-5 bg-green-100">
-            {{$storyId->storyline}}
+<div class="container mx-auto max-w-screen-lg mt-8">
+    <div class="flex flex-wrap space-x-4">
+        @foreach ($users as $user)
+
+        <div class="w-full sm:w-1/3 md:w-1/4 lg:w-1/4 h-[46vh] bg-blue-100 mb-4 p-4">
+            <a href="{{url('storyline/'.$user->id)}}" wire:navigate>
+                <div><img   src="{{ asset('storage/'. $user->image) }}" alt=""></div>
+                <h1 class="text-lg">{{$user->title}}</h1>
+                 <h1 style="color: rgb(0, 253, 63)" class="text-lg">{{$user->description}}</h1>
+            </a>
         </div>
-    @else
-        <p>Story not found.</p>
-    @endif
-    </div>
+
+        @endforeach
+    </div> 
+
 </div>
