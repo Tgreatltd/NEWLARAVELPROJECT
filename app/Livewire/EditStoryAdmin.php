c1c18590255cf00b9e147cfb560b9aad9c6ee1d7
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-11-06 05:55:14 -0400

    new commit

--- app/Livewire/EditStoryAdmin.php
+++ app/Livewire/EditStoryAdmin.php
@@ -2,10 +2,18 @@
 
 namespace App\Livewire;
 
+use App\Models\Blogger;
 use Livewire\Component;
 
 class EditStoryAdmin extends Component
 {
+    public $id, $storyId;
+
+    public function mount($id){
+     $this->id=$id;
+     $this->storyId=Blogger::find($id);
+    }
+
     public function render()
     {
         return view('livewire.edit-story-admin');
