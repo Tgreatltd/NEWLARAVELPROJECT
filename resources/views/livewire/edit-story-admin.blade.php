56cf7cf098c0f9204b6dffb67deaa7cef6765690
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-11-06 05:35:22 -0400

    new commit

--- resources/views/livewire/edit-story-admin.blade.php
+++ resources/views/livewire/edit-story-admin.blade.php
@@ -45,7 +45,7 @@
             </div>  
             
             <div class="mb-4">
-                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">Submit</button>
+                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">Update</button>
             </div>
         </form>
         
