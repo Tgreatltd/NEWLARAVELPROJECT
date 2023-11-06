5420b0962b58e818eeac5b9b2464f902af1b9d31
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-11-06 05:18:46 -0400

    new commit

--- resources/views/livewire/navbaradmin.blade.php
+++ resources/views/livewire/navbaradmin.blade.php
@@ -58,7 +58,7 @@
             </div>
                 
             <div class="pt-2 pb-3 space-y-1">
-                <x-responsive-nav-link :href="route('storyadmin')" :active="request()->routeIs('storyadmin')" wire:navigate>
+                <x-responsive-nav-link :href="route('admin')" :active="request()->routeIs('admin')" wire:navigate>
                     {{ __('STORIES') }}
                 </x-responsive-nav-link>
             </div>
