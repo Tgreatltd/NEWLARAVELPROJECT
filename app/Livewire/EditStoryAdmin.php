6d404fe8e68d4d326a4e84bf41f5ca74b047f61e
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-11-08 07:25:05 -0400

    first commit

--- app/Livewire/EditStoryAdmin.php
+++ app/Livewire/EditStoryAdmin.php
@@ -15,38 +15,40 @@ class EditStoryAdmin extends Component
     {
         $this->id = $id;
         $this->storyId = Blogger::find($id);
-        $this->title=$this->storyId->title;
-        $this->description=$this->storyId->description;
-        $this->storyline=$this->storyId->storyline;
-        $this->image=$this->storyId->image;
+        $this->title = $this->storyId->title;
+        $this->description = $this->storyId->description;
+        $this->storyline = $this->storyId->storyline;
+        $this->image = $this->storyId->image;
     }
 
     public function update()
-{
-    $data = $this->validate([
-        'title' => 'required|string',
-        'description' => 'required|string',
-        'storyline' => 'required|string',
-        'image' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
-    ]);
-    
-    if ($this->storyId) {
-        if ($this->image) {
-            // Handle the image upload and storage
-            $imagePath = $this->image->store('images', 'public');
+    {
+        $data = $this->validate([
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'storyline' => 'required|string',
+            'image' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
 
-            // Update the image path in the database
-            $this->storyId->image = $imagePath;
+        if ($this->storyId) {
+            if ($this->image) {
+                // Handle the image upload and storage
+                $imagePath = $this->image->store('images', 'public');
+
+                // Update the image path in the database
+                $this->storyId->image = $imagePath;
+            }
+
+
+            //update the edited forms
+            $this->storyId->title = $data['title'];
+            $this->storyId->description = $data['description'];
+            $this->storyId->storyline = $data['storyline'];
+            $this->storyId->save();
         }
-
-        $this->storyId->title = $data['title'];
-        $this->storyId->description = $data['description'];
-        $this->storyId->storyline = $data['storyline'];
-        $this->storyId->save();
+        session()->flash('success', 'Updated successfully');
+        return redirect()->back(); // Redirect to a specific route after the update.
     }
-    session()->flash('success', 'Updated successfully');
-    return redirect()->back(); // Redirect to a specific route after the update.
-}
 
     public function render()
     {
