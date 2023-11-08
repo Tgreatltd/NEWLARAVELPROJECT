9709bb2fe4efcadd9017de36a71ab1932ac42d50
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-11-08 08:05:49 -0400

    first commit

--- resources/views/livewire/adminpage.blade.php
+++ resources/views/livewire/adminpage.blade.php
@@ -29,7 +29,7 @@
     
             <div class="mb-4">
                 <label for="description" class="block text-gray-700 text-sm font-bold mb-2 w-full">Storyline</label>
-                <textarea wire:model="storyline" name="storyline" id="storyline" class="w-full py-2 px-3 border rounded" rows="4" placeholder="Enter storyline"></textarea>
+                <textarea wire:model="storyline" name="storyline" id="storyline" class="w-full py-2 px-3 border rounded" rows="8" placeholder="Enter storyline"></textarea>
                 @error('storyline')
                 <small>{{$message}}</small> 
              @enderror
