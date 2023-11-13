0abd64ff320549839a0a8a2d2a8841bafef63cf7
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-11-13 18:49:25 -0400

    new commit

---
+++ app/Livewire/EditNewsAdmin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class EditNewsAdmin extends Component
+{
+    public function render()
+    {
+        return view('livewire.edit-news-admin');
+    }
+}
