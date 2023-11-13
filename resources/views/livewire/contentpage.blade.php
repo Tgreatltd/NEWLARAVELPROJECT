a9f260c2a3c0954ee66691b752845773ed3c806c
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-11-13 13:23:53 -0400

    new commit

--- resources/views/livewire/contentpage.blade.php
+++ resources/views/livewire/contentpage.blade.php
@@ -9,7 +9,7 @@
                     <div class="container bg-gray-700"><img style="height:200px; width:100%" class="mb-4"   src="{{ asset('storage/'. $user->image) }}" alt="">
                         <div class=" flex items-center justify-center bg-gray-500 mb-4" style="margin-bottom:10px;height:50px;width: 100%;"><h1 class="text-white">{{$user->title}}</h1></div>
                         <div style="width: 100%; height:130px;"> <h1 class="text-white">{{$user->description}}</h1></div>
-                    </div> 
+                    </div>
                 </a>
             </div>
 
