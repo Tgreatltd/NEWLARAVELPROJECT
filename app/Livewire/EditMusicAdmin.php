09880862a0efe4b60dfd6199bbb43c0c23c6f3e2
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-11-13 18:41:09 -0400

    new commit

--- app/Livewire/EditMusicAdmin.php
+++ app/Livewire/EditMusicAdmin.php
@@ -4,14 +4,43 @@ namespace App\Livewire;
 
 use App\Models\Musicblogger;
 use Livewire\Component;
+use Livewire\Features\SupportFileUploads\WithFileUploads;
 
 class EditMusicAdmin extends Component
 {
-public $id, $title, $description, $storyline, $image, $musicId;
-public function mount($id){
- $this->$id=$id;
- $this->musicId= Musicblogger::find($id);
-}
+    use WithFileUploads;
+    public $id, $title, $description, $storyline, $image, $musicId;
+    public function mount($id)
+    {
+        $this->$id = $id;
+        $this->musicId = Musicblogger::find($id);
+        $this->title = $this->musicId->title;
+        $this->description = $this->musicId->description;
+        $this->storyline = $this->musicId->storyline;
+        $this->image = $this->musicId->image;
+    }
+
+    public function update()
+    {
+        $data = $this->validate([
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'storyline' => 'required|string',
+            'image' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        if ($this->musicId) {
+            if ($this->image) {
+                $imagePath = $this->image->store('images', 'public');
+                $this->musicId->image = $imagePath;
+            }
+
+            $this->musicId->title=$data['title'];
+            $this->musicId->description=$data['description'];
+            $this->musicId->title=$data['title'];
+            $this->musicId->title=$data['title'];
+        }
+    }
 
     public function render()
     {
