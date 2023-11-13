12b0d9b0423f64226f66abb5de719412c2848886
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-11-13 17:10:58 -0400

    new commit

--- app/Livewire/EditSportAdmin.php
+++ app/Livewire/EditSportAdmin.php
@@ -2,10 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\Sports;
 use Livewire\Component;
 
 class EditSportAdmin extends Component
 {
+    public $id, $sportId, $title, $description, $storyline;
+    public function mount($id){
+     $this->$id=$id;
+     $this->sportId= Sports::find($id);
+     $this->title=$this->sportId->title;
+     $this->description=$this->sportId->description;
+     $this->storyline=$this->sportId->storyline;
+     
+    }
+
+
     public function render()
     {
         return view('livewire.edit-sport-admin');
