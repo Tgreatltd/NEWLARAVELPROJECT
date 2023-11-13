8a0b7ca1f41248143a20ea5343bd0a3f63505798
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-11-13 17:58:42 -0400

    new commit

--- app/Livewire/EditSportAdmin.php
+++ app/Livewire/EditSportAdmin.php
@@ -4,10 +4,12 @@ namespace App\Livewire;
 
 use App\Models\Sports;
 use Livewire\Component;
+use Livewire\Features\SupportFileUploads\WithFileUploads;
 
 class EditSportAdmin extends Component
 {
-    public $id, $sportId, $title, $description, $storyline;
+    use WithFileUploads;
+    public $id, $sportId, $title, $description, $storyline, $image;
     public function mount($id){
      $this->$id=$id;
      $this->sportId= Sports::find($id);
@@ -15,6 +17,29 @@ class EditSportAdmin extends Component
      $this->description=$this->sportId->description;
      $this->storyline=$this->sportId->storyline;
      
+    }
+
+    public function update(){
+        $data = $this->validate([
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'storyline' => 'required|string',
+            'image' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+        
+        if ($this->sportId) {
+          if ($this->image) {
+            $imagePath = $this->image->store('images', 'public');
+            $this->sportId->image=$imagePath;
+          }
+
+          $this->sportId->title= $data['title'];
+          $this->sportId->description= $data['description'];
+          $this->sportId->storyline= $data['storyline'];
+          $this->sportId->save();
+          session()->flash('success', 'Updated successfully');
+        return redirect()->back(); // Redirect to a specific route after the update.
+        }
     }
 
 
