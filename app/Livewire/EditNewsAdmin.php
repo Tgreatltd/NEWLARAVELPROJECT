64798440ca1444f5963c870806a0a0fc10fffb80
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-11-13 18:57:43 -0400

    new commit

--- app/Livewire/EditNewsAdmin.php
+++ app/Livewire/EditNewsAdmin.php
@@ -2,10 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Models\News;
 use Livewire\Component;
 
 class EditNewsAdmin extends Component
 {
+    public $id, $title, $description, $newsId, $storyline, $image;
+public function mount($id){
+ $this->id=$id;
+ $this->newsId= News::find($id);
+}
+
     public function render()
     {
         return view('livewire.edit-news-admin');
