b0862f242df2115d6463d43c573c49884b3ff877
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-11-22 23:23:46 +0100

    new commit

--- app/Livewire/ContentSportPage.php
+++ app/Livewire/ContentSportPage.php
@@ -2,10 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Models\Sports;
 use Livewire\Component;
 
 class ContentSportPage extends Component
 {
+    public $sportId, $id;
+    public function mount($id)
+    {
+        $this->id = $id;
+        $this->sportId = Sports::find($id);
+    }
     public function render()
     {
         return view('livewire.content-sport-page');
