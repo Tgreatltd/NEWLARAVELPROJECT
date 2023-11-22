901f6740470d6e07d841d60d145f81c4e5504291
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-11-22 23:15:20 +0100

    new commit

--- app/Livewire/ContentMusicPage.php
+++ app/Livewire/ContentMusicPage.php
@@ -2,10 +2,18 @@
 
 namespace App\Livewire;
 
+use App\Models\Musicblogger;
 use Livewire\Component;
 
 class ContentMusicPage extends Component
 {
+
+    public $storyId, $id;
+    public function mount($id)
+    {
+        $this->id = $id;
+        $this->storyId = Musicblogger::find($id);
+    }
     public function render()
     {
         return view('livewire.content-music-page');
