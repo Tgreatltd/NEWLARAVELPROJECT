dc0472618fed5bb2ddc183b33bbdf1f9bdccbe99
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-11-22 23:29:15 +0100

    new commit

--- resources/views/livewire/content-sport-page.blade.php
+++ resources/views/livewire/content-sport-page.blade.php
@@ -1,3 +1,14 @@
-<div>
-    {{-- Do your work, then step back. --}}
+<div class="container mx-auto p-5">
+    {{-- <div>{{$storyId}}</div> --}}
+    @if ($sportId)
+    <div><img id="image" class="h-[20vh]" src="{{ asset('storage/'. $sportId->image) }}" alt=""></div>
+    <h2>{{ $sportId->title }}</h2>
+    {{-- <p>{{ $storyId->storyline }}</p> --}}
+    <div class="container mx-auto p-5 bg-green-100">
+        {{$sportId->storyline}}
+    </div>
+@else
+    <p>Story not found.</p>
+@endif
 </div>
+
