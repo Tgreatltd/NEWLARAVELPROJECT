3682d241d64641274d643f18f2bf9ccdfc53cf8f
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-11-22 23:27:32 +0100

    new commit

--- resources/views/livewire/content-music-page.blade.php
+++ resources/views/livewire/content-music-page.blade.php
@@ -1,3 +1,14 @@
-<div>
-    {{-- Be like water. --}}
+<div class="container mx-auto p-5">
+    {{-- <div>{{$storyId}}</div> --}}
+    @if ($musicId)
+    <div><img id="image" class="h-[20vh]" src="{{ asset('storage/'. $musicId->image) }}" alt=""></div>
+    <h2>{{ $musicId->title }}</h2>
+    {{-- <p>{{ $storyId->storyline }}</p> --}}
+    <div class="container mx-auto p-5 bg-green-100">
+        {{$musicId->storyline}}
+    </div>
+@else
+    <p>Story not found.</p>
+@endif
 </div>
+
--- resources/views/livewire/content-news-page.blade.php
+++ resources/views/livewire/content-news-page.blade.php
@@ -1,3 +1,13 @@
-<div>
-    {{-- Stop trying to control. --}}
+<div class="container mx-auto p-5">
+    {{-- <div>{{$storyId}}</div> --}}
+    @if ($newsId)
+    <div><img id="image" class="h-[20vh]" src="{{ asset('storage/'. $newsId->image) }}" alt=""></div>
+    <h2>{{ $newsId->title }}</h2>
+    {{-- <p>{{ $storyId->storyline }}</p> --}}
+    <div class="container mx-auto p-5 bg-green-100">
+        {{$newsId->storyline}}
+    </div>
+@else
+    <p>Story not found.</p>
+@endif
 </div>
