c0d1b040194d9a0545456323632494fb6e938399
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-11-22 23:21:52 +0100

    new commit

--- app/Livewire/ContentNewsPage.php
+++ app/Livewire/ContentNewsPage.php
@@ -2,10 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Models\News;
 use Livewire\Component;
 
 class ContentNewsPage extends Component
 {
+    public $newsId, $id;
+    public function mount($id)
+    {
+        $this->id = $id;
+        $this->newsId = News::find($id);
+    }
     public function render()
     {
         return view('livewire.content-news-page');
