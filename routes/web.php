ac18352e529fc9909f0e6deb794ba0241fb72481
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-11-23 18:43:49 +0100

    new commit

--- routes/web.php
+++ routes/web.php
@@ -28,17 +28,31 @@ Route::view('sport/{id}', 'contentsportpage')->name('sport');
 
 // admin pages routes
 
-Route::view('admin', 'storiesadminroute')->name('admin');
-Route::view('sportadmin', 'sportsadminroute')->name('sportadmin');
-Route::view('musicadmin', 'musicadminroute')->name('musicadmin');
-Route::view('newsadmin', 'newsadminroute')->name('newsadmin');
+Route::view('admin', 'storiesadminroute')
+->middleware(['auth', 'verified'])->name('admin');
+
+Route::view('sportadmin', 'sportsadminroute')
+->middleware(['auth', 'verified'])->name('sportadmin');
+
+Route::view('musicadmin', 'musicadminroute')
+->middleware(['auth', 'verified'])->name('musicadmin');
+
+Route::view('newsadmin', 'newsadminroute')
+->middleware(['auth', 'verified'])->name('newsadmin');
 
 
 // editing adminpage routes
-Route::view('storyedit/{id}', 'editStoriesadmin')->name('storyedit');
-Route::view('sportedit/{id}', 'editsportadmin')->name('sportedit');
-Route::view('musicedit/{id}', 'editMusicAdmin')->name('sportedit');
-Route::view('newsedit/{id}', 'editNewsAdmin')->name('newsedit');
+Route::view('storyedit/{id}', 'editStoriesadmin') 
+ ->middleware(['auth', 'verified'])->name('storyedit');
+
+ Route::view('sportedit/{id}', 'editsportadmin')
+->middleware(['auth', 'verified'])->name('sportedit');
+
+Route::view('musicedit/{id}', 'editMusicAdmin')
+->middleware(['auth', 'verified'])->name('sportedit');
+
+Route::view('newsedit/{id}', 'editNewsAdmin')
+->middleware(['auth', 'verified'])->name('newsedit');
 
 // Route::get('storyedit/{id}', function ($id) {
 //     return view('editStoriesadmin',compact('id'));
